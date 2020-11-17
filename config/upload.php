545d03df49dfb27b62b9ffcbd47254cbcc39e8fc
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-11-17 21:51:51 +0200

    upload img:

--- config/upload.php
+++ config/upload.php
@@ -2,5 +2,13 @@
 
 
 if(isset($_POST['upload'])){
-    header('Location: ../index.php');
+    // header('Location: ../index.php');
+    $target_dir = './uploads';
+    $target_file = $target_dir . basename($_FILES['photo']['name']);
+
+    $error = 1;
+
+    
+    move_uploaded_file($_FILES['photo']['tmp_name'], $target_file);
+    header("Location: ../index.php");
 }
