df6f99dcd73a1155811e30543830d73d81c89269
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-11-23 19:15:21 +0200

    delete event

--- crud.js
+++ crud.js
@@ -8,7 +8,7 @@ fetch('http://localhost:7000/').then(res => {
                         <div class="border-bottom p-3 my-2">
                             <div class="close btn-group">
                                 <button id="edit" data-id="${users.id}" class="btn btn-sm btn-info"><i class="fa fa-edit"></i></button>
-                                <button id="delete" data-id="${users.id}" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
+                                <a href="#" id="del" data-id="${users.id}" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
                             </div>
                             <h3>${users.name}</h3>
                             <p class="d-flex justify-content-between align-items-center flex-wrap">
@@ -36,17 +36,23 @@ function posts(e) {
     const name = document.getElementById('name').value
     const email = document.getElementById('email').value
     const phone = document.getElementById('phone').value
-    fetch('http://localhost:7000/', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json, text/plan, */*',
-            'Content-type': 'application/json'
-        },body:JSON.stringify({name,email,phone})
-    }).then((res) => res.json())
-    .then((data)=>console.log(data))
-    if (name === '' || email === '' || phone === '') {
-       error.innerHTML = '<p>There is empty</p>'
+    if (!name || !email || !phone) {
+        error.innerHTML = '<p class="alert alert-warning>Empty fields</p>'
     } else {
-        alert('Pass')
-   }  
+        fetch('http://localhost:7000/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plan, */*',
+                'Content-type': 'application/json'
+            },body:JSON.stringify({name,email,phone})
+        }).then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+            })
+        name = ''
+        email = ''
+        phone = ''
+    }
 }
+
+
