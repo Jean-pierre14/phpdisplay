8bfc047445f850eb4b4d73ecf4652550c52f8faa
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-12-09 02:34:09 +0200

    Style of form validation

--- public/js/script.js
+++ public/js/script.js
@@ -6,4 +6,30 @@ const pass = document.getElementById('pass')
 form.addEventListener('submit', function (e) {
     e.preventDefault()
     
+    checkInputs()
 })
+
+function checkInputs() {
+    const userValue = username.value.trim()
+    const emValue = email.value.trim()
+    const psValue = pass.value.trim()
+
+    // Check the username
+    if (userValue === '') {
+        // Display error
+        // Add our error class
+        setErrorFor(username, 'Username is blank')
+    } else {
+        setSuccessFor(username)
+    }
+}
+
+function setErrorFor(input, msg) {
+    const group = input.parentElement
+    const small = group.querySelector('small')
+
+    small.innerText = msg
+
+    // add the class error
+    group.className = 'group error'
+}
